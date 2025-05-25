6f9c03a36f132dee2f870f6f0b12d828b2df20c4
SinaTZ <sinatofangsaz@gmail.com>
2025-05-25 10:43:30 +0330

    feat: Adding Linting Rules - #2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
+/* eslint-disable unicorn/import-style */
+/* eslint-disable unicorn/prefer-node-protocol */
+/* eslint-disable unicorn/prefer-import-meta-properties */
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +15,8 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:drizzle/all", "prettier"),
+  eslintPluginUnicorn.configs.all,
 ];
 
 export default eslintConfig;
